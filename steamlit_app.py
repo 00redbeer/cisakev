44baab5412950d4052a1e2955a78472230194e36
00redbeer <53138844+00redbeer@users.noreply.github.com>
2023-03-21 09:23:51 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -7,10 +7,39 @@ def load_data(file):
     df = pd.read_csv(file)
     return df
 
-def create_chart(df):
-    chart = alt.Chart(df).mark_bar().encode(
-        x=alt.X('column_name', type='nominal'),
-        y='count()'
+def create_chart(df, column_name):
+    value_counts = df[column_name].value_counts()
+    most_common_value = value_counts.index[0]
+    chart_df = pd.DataFrame({column_name: [most_common_value, "Other"],
+                              "count": [value_counts[most_common_value], 
+                                        len(df) - value_counts[most_common_value]]})
+    chart = alt.Chart(chart_df).mark_bar().encode(
+        x=column_name,
+        y='count',
+        color=column_name
+    ).properties(
+        width=500,
+        height=300
+    ).transform_joinaggregate(
+        total='sum(count)',
+    ).transform_calculate(
+        percentage="datum.count / datum.total"
+    ).mark_bar().encode(
+        tooltip=['count', alt.Tooltip('percentage', format='.2%')]
+    ).properties(
+        title=f"Most Common Value in {column_name}"
+    ).mark_text(
+        align='center',
+        baseline='middle',
+        dx=0,
+        dy=5,
+        font={'size': 14}
+    ).encode(
+        text=alt.condition(
+            alt.datum.count > 0,
+            alt.datum[column_name],
+            alt.value(' ')
+        )
     )
     return chart
 
@@ -20,7 +49,8 @@ def main():
     if file:
         df = load_data(file)
         st.write(df)
-        chart = create_chart(df)
+        column_name = st.selectbox("Select a column", df.columns)
+        chart = create_chart(df, column_name)
         st.altair_chart(chart, use_container_width=True)
 
 if __name__ == '__main__':
