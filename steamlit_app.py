6ca3c792c3b184c3a69cb622f2c81068c5010a25
00redbeer <53138844+00redbeer@users.noreply.github.com>
2023-03-21 09:22:27 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,11 +1,18 @@
-
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 @st.cache
 def load_data(file):
     df = pd.read_csv(file)
     return df
+
+def create_chart(df):
+    chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X('column_name', type='nominal'),
+        y='count()'
+    )
+    return chart
 
 def main():
     st.title("Upload CSV File")
@@ -13,6 +20,8 @@ def main():
     if file:
         df = load_data(file)
         st.write(df)
+        chart = create_chart(df)
+        st.altair_chart(chart, use_container_width=True)
 
 if __name__ == '__main__':
     main()
