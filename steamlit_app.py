5ca2c8900d222670bbbb64d7a69c41a088397972
00redbeer <53138844+00redbeer@users.noreply.github.com>
2023-03-21 09:51:15 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,40 +1,33 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-def load_data():
-    file = st.file_uploader("Upload CSV file", type=["csv"])
-    if file is not None:
-        data = pd.read_csv(file)
-        return data
-def display_bar_chart(df, x, y):
+# function to create pie chart
+def create_pie_chart(df):
+    # Count the number of CVEs for each product
+    product_counts = df['product'].value_counts()
+
+    # Create the pie chart
     fig, ax = plt.subplots()
-    sns.barplot(x=x, y=y, data=df)
+    ax.pie(product_counts.values, labels=product_counts.index, autopct='%1.1f%%')
+    ax.set_title('CVEs by Product')
     st.pyplot(fig)
-def show_dashboard(data):
-    st.title("CSV Dashboard")
 
-    if data is not None:
-        # display basic info about the data
-        st.write("Shape of the data:", data.shape)
-        st.write("Columns:", data.columns)
-
-        # display data table
-        st.write("Data Table")
-        st.write(data)
-
-        # display visualization options
-        st.sidebar.title("Visualization Options")
-        chart_type = st.sidebar.selectbox("Select Chart Type", ["Bar Chart"])
-        x_column = st.sidebar.selectbox("Select X Axis", data.columns)
-        y_column = st.sidebar.selectbox("Select Y Axis", data.columns)
-
-        # display visualization
-        if chart_type == "Bar Chart":
-            display_bar_chart(data, x_column, y_column)
+# main function for Streamlit app
 def main():
-    data = load_data()
-    show_dashboard(data)
+    st.title("CVE Analysis App")
+    file = st.file_uploader("Upload CSV", type=["csv"])
+    if file is not None:
+        df = pd.read_csv(file)
+        st.write(df.head())
+
+        # Create the pie chart
+        create_pie_chart(df)
+
+        # Show recent entry and total number of CVEs
+        st.subheader("CVE Analysis Results")
+        st.write("Recent Entry: ", df.iloc[-1]['cve_id'])
+        st.write("Total Number of CVEs: ", len(df))
+
 if __name__ == "__main__":
     main()
