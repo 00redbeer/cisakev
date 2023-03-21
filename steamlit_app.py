518af7b324b4f6427a7691c254df53659cc6a7da
00redbeer <53138844+00redbeer@users.noreply.github.com>
2023-03-21 09:48:14 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,32 +3,38 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# function to display frequency distribution chart
-def show_frequency_distribution(df, column_name):
-    sns.histplot(data=df, x=column_name)
-    st.pyplot()
-
-# function to display most common values chart
-def show_most_common_values(df, column_name):
-    value_counts = df[column_name].value_counts()
-    st.bar_chart(value_counts)
-
-# main function for Streamlit app
-def main():
-    st.title("CSV Visualization App")
-    file = st.file_uploader("Upload CSV", type=["csv"])
+def load_data():
+    file = st.file_uploader("Upload CSV file", type=["csv"])
     if file is not None:
-        df = pd.read_csv(file)
-        st.write(df.head())
+        data = pd.read_csv(file)
+        return data
+def display_bar_chart(df, x, y):
+    fig, ax = plt.subplots()
+    sns.barplot(x=x, y=y, data=df)
+    st.pyplot(fig)
+def show_dashboard(data):
+    st.title("CSV Dashboard")
 
-        # let the user select the type of visualization
-        chart_type = st.selectbox("Select chart type:", ["Frequency Distribution", "Most Common Values"])
-        column_name = st.selectbox("Select column to visualize:", df.columns)
+    if data is not None:
+        # display basic info about the data
+        st.write("Shape of the data:", data.shape)
+        st.write("Columns:", data.columns)
 
-        if chart_type == "Frequency Distribution":
-            show_frequency_distribution(df, column_name)
-        elif chart_type == "Most Common Values":
-            show_most_common_values(df, column_name)
+        # display data table
+        st.write("Data Table")
+        st.write(data)
 
+        # display visualization options
+        st.sidebar.title("Visualization Options")
+        chart_type = st.sidebar.selectbox("Select Chart Type", ["Bar Chart"])
+        x_column = st.sidebar.selectbox("Select X Axis", data.columns)
+        y_column = st.sidebar.selectbox("Select Y Axis", data.columns)
+
+        # display visualization
+        if chart_type == "Bar Chart":
+            display_bar_chart(data, x_column, y_column)
+def main():
+    data = load_data()
+    show_dashboard(data)
 if __name__ == "__main__":
     main()
