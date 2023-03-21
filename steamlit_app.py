89d02b3d36d3540fad11ec37352acda7deea85f9
00redbeer <53138844+00redbeer@users.noreply.github.com>
2023-03-21 09:20:29 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+import pandas as pd
+
+@st.cache
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
+
+def main():
+    st.title("Upload CSV File")
+    file = st.file_uploader("Choose a CSV file")
+    if file:
+        df = load_data(file)
+        st.write(df)
+
+if __name__ == '__main__':
+    main()
